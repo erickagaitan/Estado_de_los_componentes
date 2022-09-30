dccd3c23676266e8a7523ac733c7a7e8fb9cf508
erickagaitan <gaitanericka@gmail.com>
2022-09-30 11:55:44 -0300

    Desafio esado de los componentes

--- my-app/src/componentes/Boton.jsx
+++ my-app/src/componentes/Boton.jsx
@@ -1,5 +1,5 @@
 const Boton = ({nombre,password})=> {
-    const handleLogin = ()=>alert('Ingresando...')
+    const handleLogin = ()=>alert('"Ingresando"...')
     return (
     <div>
     <button className={ nombre && password === "252525" ? "" : "disabled" }
